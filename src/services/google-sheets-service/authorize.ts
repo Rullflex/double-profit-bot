f921c6789afb88041a275a5eab9d3ed51ba22fb2
Dmitry Gorshkov <libafer99@gmail.com>
2025-06-22 16:48:14 +0500

    fix: sheets service auth with jwt

--- src/services/google-sheets-service/authorize.ts
+++ src/services/google-sheets-service/authorize.ts
@@ -1,52 +1,24 @@
 import fs from "fs";
 import { google } from "googleapis";
-import readline from "readline";
 import { LoggerService } from "../logger-service";
-import { OAuth2Client } from "google-auth-library";
+import path from "path";
+import { JWT } from "google-auth-library";
 
 const SCOPES = ["https://www.googleapis.com/auth/spreadsheets"];
-const TOKEN_PATH = "token.json";
 const CREDENTIALS_PATH = "credentials.json";
 
 const logger = new LoggerService("GoogleSheetsService.Authorize");
 
-export async function authorize(): Promise<OAuth2Client> {
+export async function authorize(): Promise<JWT> {
   const credentials = JSON.parse(fs.readFileSync(CREDENTIALS_PATH, "utf-8"));
-  const { client_secret, client_id, redirect_uris } = credentials.installed;
-  const oAuth2Client = new google.auth.OAuth2(
-    client_id,
-    client_secret,
-    redirect_uris[0]
-  );
 
-  if (fs.existsSync(TOKEN_PATH)) {
-    const token = JSON.parse(fs.readFileSync(TOKEN_PATH, "utf-8"));
-    oAuth2Client.setCredentials(token);
-    return oAuth2Client;
-  }
-
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: "offline",
-    scope: SCOPES,
+  const client = new google.auth.JWT({
+    email: credentials.client_email,
+    key: credentials.private_key,
+    scopes: SCOPES,
   });
 
-  logger.log("Authorize this app by visiting this URL:", authUrl);
-
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-
-  const question = (query: string) =>
-    new Promise<string>((resolve) => rl.question(query, resolve));
-
-  const code = await question("Enter the code from that page here: ");
-  rl.close();
-
-  const { tokens } = await oAuth2Client.getToken(code);
-  oAuth2Client.setCredentials(tokens);
-
-  fs.writeFileSync(TOKEN_PATH, JSON.stringify(tokens));
-  logger.log("Token stored to", TOKEN_PATH);
-  return oAuth2Client;
+  await client.authorize();
+  logger.log("Authorized using service account.");
+  return client;
 }
